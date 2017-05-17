6444eaead4c6d59e1df50e9e2d0215cf8e59eb5b
NguyenHuyLong <nguyen.huy.long@framgia.com>
2017-05-17 21:20:35 +0700

    Reset database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,13 @@
-User.create!(name: "Diem Ly Vu",
-  email: "diemly.admin@example.com",
-  password: "111111",
-  password_confirmation: "111111",
-  admin: true)
+User.create!(name: "Nguyen Huy Long",
+  email: "nguyenhuylong5695@gmail.com",
+  password: "123456",
+  password_confirmation: "123456",
+)
 
-User.create!(name: "Example User", email: "example@railstutorial.org",
-  password: "foobar", password_confirmation: "foobar", admin: true)
+User.create!(name: "Admin", email: "admin@gmail.com",
+  password: "123456", password_confirmation: "123456", admin: true)
 
-49.times do |n|
+99.times do |n|
   name = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
