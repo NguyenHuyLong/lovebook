c43bae617959b0812c91725666063890609e3472
NguyenHuyLong <nguyen.huy.long@framgia.com>
2017-05-20 15:54:22 +0700

    done

--- db/migrate/20170206092959_create_books.rb
+++ db/migrate/20170206092959_create_books.rb
@@ -7,7 +7,7 @@ class CreateBooks < ActiveRecord::Migration[5.0]
       t.string :image
       t.datetime :publish_date
       t.integer :number_of_pages
-      t.float :rating, default: 0.0
+      t.float :rating, default: 0.00
       t.references :category, foreign_key: true
 
       t.timestamps
